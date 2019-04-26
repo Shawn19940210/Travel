b2d3a03650c54aa9aaf28d6d13b443982bc967f8
Shawn19940210 <1432874828@qq.com>
2019-04-26 14:20:58 +0800

    projice init

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+import './assets/styles/reset.css'
+// 1像素边框问题
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
+// 将fastClick绑定到document.body，解决移动端click300ms网络延时问题
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
